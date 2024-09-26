d6debb667a59c2b1f35fe618c0a6d298b9c74d1a
chr8stoph <57600205+cbathon@users.noreply.github.com>
2024-09-26 12:44:05 -0700

    adjusted bridge script (shrink behavior)

--- Assets/Scripts/BridgeControl.cs
+++ Assets/Scripts/BridgeControl.cs
@@ -5,22 +5,16 @@ using UnityEngine;
 public class BridgeControl : MonoBehaviour
 {
     // Speed at which the object shrinks
-    public float initialShrinkSpeed;
-    public float minShrinkSpeed;  // Minimum shrink speed
-    public float speedDecreasePerPress ;  // How much to decrease speed per key press
+    public float shrinkSpeed;
+    public float widthIncreasePerPress;  // How much to increase width per key press
 
-    private float currentShrinkSpeed;
-    private bool shouldShrink = false;
     private bool isJKeyPressed = false;
-
     private bool isActive = false;
-
 
     // Update is called once per frame
     void Start()
     {
-        currentShrinkSpeed = initialShrinkSpeed;
-        Debug.Log($"BridgeControl started. Initial shrink speed: {currentShrinkSpeed}");
+        Debug.Log($"BridgeControl started. Shrink speed: {shrinkSpeed}");
     }
 
     void Update()
@@ -33,44 +27,36 @@ public class BridgeControl : MonoBehaviour
         else if (Input.GetKeyUp(KeyCode.J))
         {
             isJKeyPressed = false;
-            ResetSpeed();
-            Debug.Log("J key released, speed reset to normal");
+            Debug.Log("J key released");
         }
 
         // Shrinking logic
         if (isActive) {
-            if (isJKeyPressed)
-            {
-                DecreaseSpeed();
-            }
-
             // Get the current scale of the object
             Vector3 currentScale = transform.localScale;
+
             // Reduce the x scale over time
-            float newXScale = Mathf.Max(0, currentScale.x - currentShrinkSpeed * Time.deltaTime);
-            // Apply the new scale to the object
+            float newXScale = Mathf.Max(0, currentScale.x - shrinkSpeed * Time.deltaTime);
             transform.localScale = new Vector3(newXScale, currentScale.y, currentScale.z);
+            Debug.Log($"Shrinking. Current scale: {transform.localScale}");
+
+            if (isJKeyPressed)
+            {
+                // Add width when J key is pressed
+                float newWidth = currentScale.x + widthIncreasePerPress * Time.deltaTime;
+                transform.localScale = new Vector3(newWidth, currentScale.y, currentScale.z);
+                Debug.Log($"Width increased. Current scale: {transform.localScale}");
+            }
+
             // If the x scale is zero, disable the object
-            if (newXScale == 0)
+            if (transform.localScale.x == 0)
             {
                 Debug.Log("Bridge fully shrunk, disabling object");
                 gameObject.SetActive(false); // Disable the bridge
             }
         } else {
-            Debug.Log("Not shrinking. shouldShrink is false.");
+            Debug.Log("Not shrinking. isActive is false.");
         }
-    }
-
-    private void DecreaseSpeed()
-    {
-        currentShrinkSpeed = Mathf.Max(minShrinkSpeed, currentShrinkSpeed - speedDecreasePerPress * Time.deltaTime);
-        Debug.Log($"Decreasing speed. Current shrink speed: {currentShrinkSpeed}");
-    }
-
-    private void ResetSpeed()
-    {
-        currentShrinkSpeed = initialShrinkSpeed;
-        Debug.Log($"Speed reset to normal: {currentShrinkSpeed}");
     }
 
     public void SetActive(bool active)

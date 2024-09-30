84182222be12c243c57dcfa89a592ded62655fa8
chr8stoph <57600205+cbathon@users.noreply.github.com>
2024-09-29 20:44:14 -0700

    polished levels and adjusted parameters

--- Assets/Scripts/HUDManager.cs
+++ Assets/Scripts/HUDManager.cs
@@ -18,17 +18,15 @@ public class HUDManager : MonoBehaviour
     private void UpdatePlayer1HUD()
     {
         string controls = ballControl.onBridge ? "[ WASD ]  Move" : "[ Space ]  Jump";
-        string colorTag = ballControl.onBridge ? "<color=#6699FF>" : "<color=#B3CCFF>"; // Light blue color when on bridge, lighter blue when on platform
-        string closeColorTag = "</color>";
-        player1HUD.text = $"{colorTag}Player 1\n<size=60%>{controls}</size>{closeColorTag}";
+        Color ballColor = ballControl.GetComponent<Renderer>().material.color;
+        string colorHex = ColorUtility.ToHtmlStringRGB(ballColor);
+        player1HUD.text = $"<color=#{colorHex}>Player 1\n<size=60%>{controls}</size></color>";
     }
 
     private void UpdatePlayer2HUD()
     {
         string controls = !ballControl.onBridge ? "[ Arrows ]  Tilt" : "[ J ]  Stop Shrinking";
-        string colorTag = ballControl.onBridge ? "<color=#B2FFB2>" : "<color=#66CC66>"; // Light green when on bridge, light green when not on bridge (platform active)
-        player2HUD.text = $"{colorTag}Player 2\n<size=60%>{controls}</size>";
-        string closeColorTag = !ballControl.onBridge ? "</color>" : "";
-        player2HUD.text = $"{colorTag}Player 2\n<size=60%>{controls}</size>{closeColorTag}";
+        string colorTag = ballControl.onBridge ? "<color=#B2FFB2>" : "<color=#66CC66>"; // Light green when on bridge, darker green when not on bridge (platform active)
+        player2HUD.text = $"{colorTag}Player 2\n<size=60%>{controls}</size></color>";
     }
 }
